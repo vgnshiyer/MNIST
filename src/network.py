255b03ec458f873aee90580ede81488f18b6f71e
Vignesh Iyer <vgnshiyer@gmail.com>
2026-03-24 16:37:37 -0700

    Remove MNIST dataset files and add mnist_loader for data handling

--- src/network.py
+++ src/network.py
@@ -1,19 +1,31 @@
-import numpy as np
+"""
+
+"""
+
 import random
+import numpy as np
 
 
 class Network:
     def __init__(self, sizes):
+        """The list ``sizes`` contains the number of neurons in the
+        respective layers of the network.  For example, if the list
+        was [2, 3, 1] then it would be a three-layer network, with the
+        first layer containing 2 neurons, the second layer 3 neurons,
+        and the third layer 1 neuron.  The biases and weights for the
+        network are initialized randomly, using a Gaussian
+        distribution with mean 0, and variance 1.  Note that the first
+        layer is assumed to be an input layer, and by convention we
+        won't set any biases for those neurons, since biases are only
+        ever used in computing the outputs from later layers."""
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
-    def sigmoid(self, z):
-        return 1.0 / 1.0 + np.exp(-z)
-
     def feedforward(self, a):
+        """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
             a = self.sigmoid(np.dot(w, a) + b)
         return a
@@ -56,4 +68,7 @@ class Network:
             nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
         self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
-        
+
+
+def sigmoid(self, z):
+    return 1.0 / 1.0 + np.exp(-z)
